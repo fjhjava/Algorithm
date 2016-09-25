c543783a039c4ef2f22b483c3c80206618384495
fjhjava <fjh_github_io@163.com>
2016-09-25 11:36:42 +0800

    update

--- src/com/algorithm/test/TestBinarySort.java
+++ src/com/algorithm/test/TestBinarySort.java
@@ -3,15 +3,18 @@ package com.algorithm.test;
 import com.algorithm.sort.BinarySort;
 
 public class TestBinarySort {
-	//测试二分查找
+	// 测试二分查找
 	public static void main(String[] args) {
-		int[] array={1,2,3,4,5,6,7,8};
-		int[] array2={10,200,3,-2,5,6,2000,8};
-		BinarySort bs=new BinarySort();
-		int r=bs.binarySearch(array, 4);
-		System.out.println("4位置在-->"+r);
-		int r2=bs.binarySearch(array2, 6);
-		System.out.println("6位置在-->"+r2);
+		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int[] array2 = { 10, 200, 3, -2, 5, 6, 2000, 8 };
+		BinarySort bs = new BinarySort();
+		int r = bs.binarySearch(array, 4);
+		System.out.println("4位置在-->" + r);
+		int r2 = bs.binarySearch(array2, 6);
+		System.out.println("6位置在-->" + r2);
+		// 测试-1的情况
+		int r3 = bs.binarySearch(array2, 1);
+		System.out.println("1位置在-->" + r3);
 	}
 
 }
